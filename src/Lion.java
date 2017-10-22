5740dcb873319f39a048a8e6f7bb780b9e95ae26
Dylan Vu <dylan.n.vu@gmail.com>
2017-10-22 16:55:02 -0700

    Add Lion class

---
+++ src/Lion.java
@@ -0,0 +1,9 @@
+public class Lion extends Animal {
+    public Lion() {
+        super();
+    }
+
+    public void talk() {
+        System.out.println("Roar!");
+    }
+}
--- src/Zoo.java
+++ src/Zoo.java
@@ -1,7 +1,13 @@
 public class Zoo {
     public static void main(String[] args) {
         Zebra joe = new Zebra();
-        System.out.println("Hunger: " + joe.getHunger());
+        System.out.println("Zebra Joe's Hunger: " + joe.getHunger());
         joe.talk();
+
+        System.out.println();
+
+        Lion simba = new Lion();
+        System.out.println("Lion Simba's Hunger: " + simba.getHunger());
+        simba.talk();
     }
 }
