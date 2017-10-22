011d8a5065387675059c2ce17d176869f79e1e98
Dylan Vu <dylan.n.vu@gmail.com>
2017-10-22 16:39:15 -0700

    Add Zoo client class

---
+++ src/Zoo.java
@@ -0,0 +1,7 @@
+public class Zoo {
+    public static void main(String[] args) {
+        Zebra joe = new Zebra();
+        System.out.println("Hunger: " + joe.getHunger());
+        joe.talk();
+    }
+}
